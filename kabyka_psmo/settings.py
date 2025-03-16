55f80a54d4bcf83b50ef93f591082c50333c5758
hisham <hisham@kiebot.com>
2025-03-16 14:36:55 +0530

    Revert "design"
    This reverts commit ed77d1195daa29bde5b37f0e2e403708317b9536.

--- kabyka_psmo/settings.py
+++ kabyka_psmo/settings.py
@@ -64,9 +64,7 @@ ROOT_URLCONF = 'kabyka_psmo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,16 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
-# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Whitenoise configuration for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- kabyka_psmo/urls.py
+++ kabyka_psmo/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
-from events.admin import admin_site  # Import our custom admin site
 
 urlpatterns = [
-    path('admin/', admin_site.urls),  # Use our custom admin site instead of default
+    path('admin/', admin.site.urls),
     path('', include('events.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
